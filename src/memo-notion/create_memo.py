e86862bef67cfc4fd68ef6e31cdf79505170091a
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2024-02-15 04:35:46 +0000

    create devcontainer

--- src/memo-notion/create_memo.py
+++ src/memo-notion/create_memo.py
@@ -1,34 +1,56 @@
 from notion_client import Client
+import os
+import dotenv
+dotenv.load_dotenv()
+
 
 # ノーションAPIキーを設定
-notion = Client(auth="あなたのノーションAPIキー")
+notion_api_key = os.getenv("NOTION_API_KEY")
+notion = Client(auth=notion_api_key)
 
 # ページを追加するデータベースのID
-database_id = "あなたのデータベースID"
+database_id = os.getenv("test_db")
 
 # データベースに新しいページを追加
 new_page = notion.pages.create(
-    parent={"database_id": database_id},
-    properties={
-        "Name": {
-            "title": [
-                {
-                    "text": {
-                        "content": "ChatGPTとの会話"
+    **{
+        "parent": {"database_id": database_id},
+        "properties": {
+            "Title": {
+                "title": [
+                    {
+                        "text": {
+                            "content": "ChatGPTとの会話"
+                        }
                     }
-                }
-            ]
+                ]
+            },
         },
-        "Description": {
-            "rich_text": [
-                {
-                    "text": {
-                        "content": "これはテストの記録です。"
-                    }
+        "children": [
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "text": {
+                                "content": "これはテストの記録です。"
+                            }
+                        }
+                    ]
                 }
-            ]
-        }
+            },
+        ]
+        # "children": {
+        #     "rich_text": [
+        #         {
+        #             "text": {
+        #                 "content": "# これはテストの記録です。"
+        #             }
+        #         }
+        #     ]
+        # }
     }
 )
-独自
+
 print("新しいページが追加されました:", new_page)
