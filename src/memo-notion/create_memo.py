92a7e682c23ade8aeb92ebed941e9deec2537fa9
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2024-02-15 05:56:29 +0000

    test data

--- src/memo-notion/create_memo.py
+++ src/memo-notion/create_memo.py
@@ -1,7 +1,8 @@
-from notion_client import Client
 import os
+from pprint import pprint
 import dotenv
 dotenv.load_dotenv()
+from notion_client import Client
 
 
 # ノーションAPIキーを設定
@@ -9,14 +10,32 @@ notion_api_key = os.getenv("NOTION_API_KEY")
 notion = Client(auth=notion_api_key)
 
 # ページを追加するデータベースのID
-database_id = os.getenv("test_db")
+database_id = os.getenv("NOTION_DATABASE_ID")
+
+# データベース接続確認
+# db = notion.databases.query(
+#     **{
+#         'database_id' : database_id  # データベースID
+#        }
+# )
+# pprint(db)
 
 # データベースに新しいページを追加
+
+TEST_TXT = """
+# test
+
+## test2
+
+aaea
+
+"""
+
 new_page = notion.pages.create(
     **{
         "parent": {"database_id": database_id},
         "properties": {
-            "Title": {
+            "名前": {
                 "title": [
                     {
                         "text": {
@@ -34,7 +53,7 @@ new_page = notion.pages.create(
                     "rich_text": [
                         {
                             "text": {
-                                "content": "これはテストの記録です。"
+                                "content": TEST_TXT
                             }
                         }
                     ]
